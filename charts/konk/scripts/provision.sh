26a84e32a1a9bd04590bbca12ffe62a7d5fbb93d
Tom Hayward <thayward@infoblox.com>
2020-09-04 12:25:37 -0700

    Add test for apiserver with kubeconfig

--- charts/konk/scripts/provision.sh
+++ charts/konk/scripts/provision.sh
@@ -4,13 +4,21 @@ kubeadm init phase certs all
 kubeadm init phase kubeconfig admin
 if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-etcd-cert
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-etcd-cert --from-file=/etc/kubernetes/pki/etcd/ca.crt --from-file=/etc/kubernetes/pki/etcd/server.crt --from-file=/etc/kubernetes/pki/etcd/server.key
+  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-etcd-cert \
+    --from-file=/etc/kubernetes/pki/etcd/ca.crt \
+    --from-file=/etc/kubernetes/pki/etcd/server.crt \
+    --from-file=/etc/kubernetes/pki/etcd/server.key
 fi
 if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-apiserver-cert
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-apiserver-cert --from-file=/etc/kubernetes/pki/etcd/ca.crt --from-file=/etc/kubernetes/pki/apiserver-etcd-client.crt --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
+  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-apiserver-cert \
+    --from-file=/etc/kubernetes/pki/ca.crt \
+    --from-file=etcd-ca.crt=/etc/kubernetes/pki/etcd/ca.crt \
+    --from-file=/etc/kubernetes/pki/apiserver-etcd-client.crt \
+    --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
 fi
 if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-kubeconfig
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-kubeconfig --from-file=/etc/kubernetes/admin.conf
+  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-kubeconfig \
+    --from-file=/etc/kubernetes/admin.conf
 fi
