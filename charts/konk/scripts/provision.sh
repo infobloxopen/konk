7265f4ae6225afab31fb75ce0b8c0801335ab27e
Tom Hayward <thayward@infoblox.com>
2020-09-08 12:47:12 -0700

    use environment variables to eliminate requirement of templating inside scripts (#13)

--- charts/konk/scripts/provision.sh
+++ charts/konk/scripts/provision.sh
@@ -2,23 +2,23 @@
 
 kubeadm init phase certs all
 kubeadm init phase kubeconfig admin
-if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-etcd-cert
+if ! kubectl -n $NAMESPACE get secret $FULLNAME-etcd-cert
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-etcd-cert \
+  kubectl -n $NAMESPACE create secret generic $FULLNAME-etcd-cert \
     --from-file=/etc/kubernetes/pki/etcd/ca.crt \
     --from-file=/etc/kubernetes/pki/etcd/server.crt \
     --from-file=/etc/kubernetes/pki/etcd/server.key
 fi
-if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-apiserver-cert
+if ! kubectl -n $NAMESPACE get secret $FULLNAME-apiserver-cert
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-apiserver-cert \
+  kubectl -n $NAMESPACE create secret generic $FULLNAME-apiserver-cert \
     --from-file=/etc/kubernetes/pki/ca.crt \
     --from-file=etcd-ca.crt=/etc/kubernetes/pki/etcd/ca.crt \
     --from-file=/etc/kubernetes/pki/apiserver-etcd-client.crt \
     --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
 fi
-if ! kubectl -n {{ .Release.Namespace }} get secret {{ include "konk.fullname" . }}-kubeconfig
+if ! kubectl -n $NAMESPACE get secret $FULLNAME-kubeconfig
 then
-  kubectl -n {{ .Release.Namespace }} create secret generic {{ include "konk.fullname" . }}-kubeconfig \
+  kubectl -n $NAMESPACE create secret generic $FULLNAME-kubeconfig \
     --from-file=/etc/kubernetes/admin.conf
 fi
