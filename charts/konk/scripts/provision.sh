02c84866aaaf7aa77bc47bde4f227ff2055ab613
Tom Hayward <thayward@infoblox.com>
2020-09-08 14:58:49 -0700

    label secrets

--- charts/konk/scripts/provision.sh
+++ charts/konk/scripts/provision.sh
@@ -8,6 +8,7 @@ then
     --from-file=/etc/kubernetes/pki/etcd/ca.crt \
     --from-file=/etc/kubernetes/pki/etcd/server.crt \
     --from-file=/etc/kubernetes/pki/etcd/server.key
+  kubectl -n $NAMESPACE label secret $FULLNAME-etcd-cert $LABELS
 fi
 if ! kubectl -n $NAMESPACE get secret $FULLNAME-apiserver-cert
 then
@@ -16,9 +17,11 @@ then
     --from-file=etcd-ca.crt=/etc/kubernetes/pki/etcd/ca.crt \
     --from-file=/etc/kubernetes/pki/apiserver-etcd-client.crt \
     --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
+  kubectl -n $NAMESPACE label secret $FULLNAME-apiserver-cert $LABELS
 fi
 if ! kubectl -n $NAMESPACE get secret $FULLNAME-kubeconfig
 then
   kubectl -n $NAMESPACE create secret generic $FULLNAME-kubeconfig \
     --from-file=/etc/kubernetes/admin.conf
+  kubectl -n $NAMESPACE label secret $FULLNAME-kubeconfig $LABELS
 fi
