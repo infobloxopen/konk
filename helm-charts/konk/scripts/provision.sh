cdef5ae256b3bf19097104415995e1c87350c456
Tom Hayward <thayward@infoblox.com>
2020-09-10 17:30:01 -0700

    use a loop for avoid repetition

--- helm-charts/konk/scripts/provision.sh
+++ helm-charts/konk/scripts/provision.sh
@@ -33,6 +33,7 @@ fi
 kubectl -n $NAMESPACE wait --timeout=3m --for=condition=available deployment -l app.kubernetes.io/instance=$FULLNAME
 
 DEPLOYMENT_UID=$(kubectl get deploy -n $NAMESPACE $FULLNAME -o jsonpath='{.metadata.uid}')
-kubectl patch -n $NAMESPACE secret $FULLNAME-apiserver-cert -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
-kubectl patch -n $NAMESPACE secret $FULLNAME-etcd-cert -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
-kubectl patch -n $NAMESPACE secret $FULLNAME-kubeconfig -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
+for name in apiserver-cert etcd-cert kubeconfig
+do
+  kubectl patch -n $NAMESPACE secret $FULLNAME-$name -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
+done
