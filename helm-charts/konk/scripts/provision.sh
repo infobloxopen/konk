0690571273e67970daa77d5d3dd6b38a13d80fed
Ankhit Bala Venkata <abalavenkata@infoblox.com>
2020-09-23 15:51:44 -0700

    Added Issuer

--- helm-charts/konk/scripts/provision.sh
+++ helm-charts/konk/scripts/provision.sh
@@ -23,6 +23,14 @@ then
     --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
   kubectl -n $NAMESPACE label secret $FULLNAME-apiserver-cert $LABELS
 fi
+if ! kubectl -n $NAMESPACE get secret $FULLNAME-ca
+then
+  kubectl -n $NAMESPACE create secret tls $FULLNAME-ca \
+    --cert=/etc/kubernetes/pki/ca.crt \
+    --key=/etc/kubernetes/pki/ca.key
+  kubectl -n $NAMESPACE label secret $FULLNAME-ca $LABELS
+fi
+
 if ! kubectl -n $NAMESPACE get secret $FULLNAME-kubeconfig
 then
   kubectl -n $NAMESPACE create secret generic $FULLNAME-kubeconfig \
@@ -33,7 +41,7 @@ fi
 kubectl -n $NAMESPACE wait --timeout=3m --for=condition=progressing deployments.apps -l app.kubernetes.io/instance=$FULLNAME
 
 DEPLOYMENT_UID=$(kubectl get deployments.apps -n $NAMESPACE $FULLNAME -o jsonpath='{.metadata.uid}')
-for name in apiserver-cert etcd-cert kubeconfig
+for name in apiserver-cert etcd-cert ca kubeconfig
 do
   kubectl patch -n $NAMESPACE secret $FULLNAME-$name -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
 done
