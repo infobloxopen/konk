482e4f0a87c25faad9d6274e86c043120ffa9cf8
Drew Wells <drew.wells00@gmail.com>
2020-10-06 10:35:09 -0500

    generated stuff

--- test/apiserver/cmd/apiserver/main.go
+++ test/apiserver/cmd/apiserver/main.go
@@ -1,16 +1,12 @@
-
-
-
-
 package main
 
 import (
 	// Make sure dep tools picks up these dependencies
-	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
 	_ "github.com/go-openapi/loads"
+	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"sigs.k8s.io/apiserver-builder-alpha/pkg/cmd/server"
 	_ "k8s.io/client-go/plugin/pkg/client/auth" // Enable cloud provider auth
+	"sigs.k8s.io/apiserver-builder-alpha/pkg/cmd/server"
 
 	"github.com/infobloxopen/konk/test/apiserver/pkg/apis"
 	"github.com/infobloxopen/konk/test/apiserver/pkg/openapi"
@@ -21,11 +17,11 @@ func main() {
 	version := "v0"
 
 	err := server.StartApiServerWithOptions(&server.StartOptions{
-		EtcdPath:         "/registry/example.infoblox.com",
-		Apis:             apis.GetAllApiBuilders(),
-		Openapidefs:      openapi.GetOpenAPIDefinitions,
-		Title:            "Api",
-		Version:          version,
+		EtcdPath:    "/registry/example.infoblox.com",
+		Apis:        apis.GetAllApiBuilders(),
+		Openapidefs: openapi.GetOpenAPIDefinitions,
+		Title:       "Api",
+		Version:     version,
 
 		// TweakConfigFuncs []func(apiServer *apiserver.Config) error
 		// FlagConfigFuncs []func(*cobra.Command) error
