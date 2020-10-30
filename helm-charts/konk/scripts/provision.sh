1be094956b55ee781c0d089cc702787682ba8705
Drew Wells <drew.wells00@gmail.com>
2020-10-30 10:21:47 -0500

    enable delegate auth and init controller manager
    - renamed konk to example
    - provision proxy client cert for extension apiserver
    - create single selfSigned issuer for nginx and proxy client certs
    - document aggregate flags
    - make github build pod lookups more generic labels
    - rename kubeconfig secret to include entire release as prefix
    - increase debugging on pr failures

--- helm-charts/konk/scripts/provision.sh
+++ helm-charts/konk/scripts/provision.sh
@@ -31,17 +31,17 @@ then
   kubectl -n $NAMESPACE label secret $FULLNAME-ca $LABELS
 fi
 
-if ! kubectl -n $NAMESPACE get secret $RELEASE-kubeconfig
+if ! kubectl -n $NAMESPACE get secret $FULLNAME-kubeconfig
 then
-  kubectl -n $NAMESPACE create secret generic $RELEASE-kubeconfig \
+  kubectl -n $NAMESPACE create secret generic $FULLNAME-kubeconfig \
     --from-file=/etc/kubernetes/admin.conf
-  kubectl -n $NAMESPACE label secret $RELEASE-kubeconfig $LABELS
+  kubectl -n $NAMESPACE label secret $FULLNAME-kubeconfig $LABELS
 fi
 
 kubectl -n $NAMESPACE wait --timeout=3m --for=condition=progressing deployments.apps -l app.kubernetes.io/instance=$RELEASE
 
 DEPLOYMENT_UID=$(kubectl get deployments.apps -n $NAMESPACE $FULLNAME -o jsonpath='{.metadata.uid}')
-for name in $FULLNAME-apiserver-cert $FULLNAME-etcd-cert $FULLNAME-ca $RELEASE-kubeconfig
+for name in $FULLNAME-apiserver-cert $FULLNAME-etcd-cert $FULLNAME-ca $FULLNAME-kubeconfig
 do
   kubectl patch -n $NAMESPACE secret $name -p '{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1", "kind":"Deployment", "name":"'${FULLNAME}'", "uid":"'${DEPLOYMENT_UID}'"}]}}'
 done
