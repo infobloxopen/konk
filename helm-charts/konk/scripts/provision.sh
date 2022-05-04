c932f3ecd694aa49692a1e8dfff16707b6c49f01
Tom Hayward <thayward@infoblox.com>
2022-05-04 08:41:24 -0700

    WIP: apiserver-cert

--- helm-charts/konk/scripts/provision.sh
+++ helm-charts/konk/scripts/provision.sh
@@ -40,24 +40,6 @@ rm -f /etc/kubernetes/pki/etcd/server*
 kubeadm init phase certs etcd-server --config=/tmp/kubeadmcfg.yaml
 kubeadm init phase kubeconfig admin --control-plane-endpoint $FULLNAME.$NAMESPACE.svc
 find /etc/kubernetes/pki
-if secret_not_found $FULLNAME-apiserver-cert
-then
-  kubectl -n $NAMESPACE create secret generic $FULLNAME-apiserver-cert \
-    --from-file=/etc/kubernetes/pki/apiserver.crt \
-    --from-file=/etc/kubernetes/pki/apiserver.key \
-    --from-file=/etc/kubernetes/pki/ca.crt \
-    --from-file=etcd-ca.crt=/etc/kubernetes/pki/etcd/ca.crt \
-    --from-file=/etc/kubernetes/pki/apiserver-etcd-client.crt \
-    --from-file=/etc/kubernetes/pki/apiserver-etcd-client.key
-  kubectl -n $NAMESPACE label secret $FULLNAME-apiserver-cert $LABELS
-else
-  kubectl -n $NAMESPACE patch secret $FULLNAME-apiserver-cert --type=json -p '[
-    {"op":"replace","path":"/data/apiserver.crt","value":"'"$(base64 --wrap=0 < /etc/kubernetes/pki/apiserver.crt)"'"},
-    {"op":"replace","path":"/data/apiserver.key","value":"'"$(base64 --wrap=0 < /etc/kubernetes/pki/apiserver.key)"'"},
-    {"op":"replace","path":"/data/apiserver-etcd-client.crt","value":"'"$(base64 --wrap=0 < /etc/kubernetes/pki/apiserver-etcd-client.crt)"'"},
-    {"op":"replace","path":"/data/apiserver-etcd-client.key","value":"'"$(base64 --wrap=0 < /etc/kubernetes/pki/apiserver-etcd-client.key)"'"}
-  ]'
-fi
 
 if secret_not_found $FULLNAME-ca
 then
