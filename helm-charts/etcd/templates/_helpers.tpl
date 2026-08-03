infobloxopen/konk

commit 7c137577215e5908805c28a1e9d257bdbf850e34

PTCI-3293: Fix etcd vulnerabilities by replacing deprecated bitnamilegacy/etcd with upstream gcr.io/etcd-development/etcd:v3.6.7 (#572)
* Replace Bitnami etcd chart with custom chart using upstream etcd image

- Fix etcd vulnerabilities by replacing Bitnami etcd with upstream etcd image
- Add custom etcd helm chart with proper configuration
- Fix etcd health probes to work with TLS client authentication

* update chart readme

---------

Co-authored-by: vbuddhavarapu-infoblox <vbuddhavarapu-infoblox@users.noreply.github.com>

helm-charts/etcd/Chart.yaml
@@ -1,28 +1,20 @@
-annotations:
-  category: Database
 apiVersion: v2
-appVersion: 3.5.25
-dependencies:
-- name: common
-  repository: https://charts.bitnami.com/bitnami
-  tags:
-  - bitnami-common
-  version: 2.x.x
-description: etcd is a distributed key value store that provides a reliable way to
-  store data across a cluster of machines
-home: https://github.com/bitnami/charts/tree/master/bitnami/etcd
-icon: https://bitnami.com/assets/stacks/etcd/img/etcd-stack-110x117.png
-keywords:
-- etcd
-- cluster
-- database
-- cache
-- key-value
-maintainers:
-- email: containers@bitnami.com
-  name: Bitnami
 name: etcd
+description: A custom etcd Helm chart using upstream etcd images
+version: 1.0.0
+appVersion: "3.5.17"
+type: application
+
+keywords:
+  - etcd
+  - cluster
+  - database
+  - key-value
+
+home: https://etcd.io
 sources:
-- https://github.com/bitnami/bitnami-docker-etcd
-- https://coreos.com/etcd/
-version: 5.3.2
+  - https://github.com/etcd-io/etcd
+
+maintainers:
+  - name: Infoblox
+    email: cto-team@infoblox.com

helm-charts/etcd/templates/_helpers.tpl
@@ -1,53 +1,54 @@
 {{/* vim: set filetype=mustache: */}}
+
 {{/*
 Expand the name of the chart.
 */}}
 {{- define "etcd.name" -}}
-{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
 
 {{/*
 Create a default fully qualified app name.
-We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 */}}
 {{- define "etcd.fullname" -}}
-{{- if .Values.fullnameOverride -}}
-{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
-{{- else -}}
-{{- $name := default .Chart.Name .Values.nameOverride -}}
-{{- if contains $name .Release.Name -}}
-{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
-{{- else -}}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
-{{- end -}}
-{{- end -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
 
 {{/*
 Create chart name and version as used by the chart label.
 */}}
 {{- define "etcd.chart" -}}
-{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
 
 {{/*
 Common labels
 */}}
 {{- define "etcd.labels" -}}
-app.kubernetes.io/name: {{ include "etcd.name" . }}
 helm.sh/chart: {{ include "etcd.chart" . }}
-app.kubernetes.io/instance: {{ .Release.Name }}
+{{ include "etcd.selectorLabels" . }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
 app.kubernetes.io/managed-by: {{ .Release.Service }}
-{{- end -}}
+{{- end }}
 
 {{/*
-Labels to use on deploy.spec.selector.matchLabels and svc.spec.selector
+Selector labels
 */}}
-{{- define "etcd.matchLabels" -}}
+{{- define "etcd.selectorLabels" -}}
 app.kubernetes.io/name: {{ include "etcd.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
-{{- end -}}
-
+{{- end }}
 
 {{/*
 Return the proper etcd image name
@@ -56,11 +57,6 @@ Return the proper etcd image name
 {{- $registryName := .Values.image.registry -}}
 {{- $repositoryName := .Values.image.repository -}}
 {{- $tag := .Values.image.tag | toString -}}
-{{/*
-Helm 2.11 supports the assignment of a value to a variable defined in a different scope,
-but Helm 2.9 and 2.10 doesn't support it, so we need to implement this if-else logic.
-Also, we can't use a single if because lazy evaluation is not an option
-*/}}
 {{- if .Values.global }}
     {{- if .Values.global.imageRegistry }}
         {{- printf "%s/%s:%s" .Values.global.imageRegistry $repositoryName $tag -}}
@@ -73,7 +69,14 @@ Also, we can't use a single if because lazy evaluation is not an option
 {{- end -}}
 
 {{/*
-Return the proper etcd peer protocol
+Return the headless service name
+*/}}
+{{- define "etcd.headlessServiceName" -}}
+{{- printf "%s-headless" (include "etcd.fullname" .) -}}
+{{- end -}}
+
+{{/*
+Return the peer protocol (http or https)
 */}}
 {{- define "etcd.peerProtocol" -}}
 {{- if .Values.auth.peer.secureTransport -}}
@@ -84,7 +87,7 @@ Return the proper etcd peer protocol
 {{- end -}}
 
 {{/*
-Return the proper etcd client protocol
+Return the client protocol (http or https)
 */}}
 {{- define "etcd.clientProtocol" -}}
 {{- if .Values.auth.client.secureTransport -}}
@@ -95,198 +98,71 @@ Return the proper etcd client protocol
 {{- end -}}
 
 {{/*
-Return the proper Disaster Recovery PVC name
+Create the initial cluster string
 */}}
-{{- define "etcd.disasterRecovery.pvc.name" -}}
-{{- if .Values.disasterRecovery.pvc.existingClaim -}}
-{{- with .Values.disasterRecovery.pvc.existingClaim -}}
-{{ tpl . $ }}
+{{- define "etcd.initialCluster" -}}
+{{- $fullname := include "etcd.fullname" . -}}
+{{- $headlessService := include "etcd.headlessServiceName" . -}}
+{{- $releaseNamespace := .Release.Namespace -}}
+{{- $clusterDomain := .Values.clusterDomain -}}
+{{- $peerPort := int .Values.service.peerPort -}}
+{{- $peerProtocol := include "etcd.peerProtocol" . -}}
+{{- $replicaCount := int .Values.replicaCount -}}
+{{- $members := list -}}
+{{- range $i := until $replicaCount -}}
+{{- $members = append $members (printf "%s-%d=%s://%s-%d.%s.%s.svc.%s:%d" $fullname $i $peerProtocol $fullname $i $headlessService $releaseNamespace $clusterDomain $peerPort) -}}
 {{- end -}}
-{{- else -}}
-{{ template "etcd.fullname" . }}-snapshotter
-{{- end -}}
+{{- join "," $members -}}
 {{- end -}}
 
 {{/*
-Return the proper etcdctl authentication options
+Create the endpoints list for etcdctl
 */}}
-{{- define "etcd.authOptions" -}}
-{{- $rbacOption := "--user root:$ETCD_ROOT_PASSWORD" -}}
-{{- $certsOption := " --cert $ETCD_CERT_FILE --key $ETCD_KEY_FILE" -}}
-{{- $autoCertsOption := " --cert /bitnami/etcd/data/fixtures/client/cert.pem --key /bitnami/etcd/data/fixtures/client/key.pem" -}}
-{{- $caOption := " --cacert $ETCD_TRUSTED_CA_FILE" -}}
-{{- if .Values.auth.rbac.enabled -}}
-{{- printf "%s" $rbacOption -}}
+{{- define "etcd.endpoints" -}}
+{{- $fullname := include "etcd.fullname" . -}}
+{{- $headlessService := include "etcd.headlessServiceName" . -}}
+{{- $releaseNamespace := .Release.Namespace -}}
+{{- $clusterDomain := .Values.clusterDomain -}}
+{{- $clientPort := int .Values.service.port -}}
+{{- $clientProtocol := include "etcd.clientProtocol" . -}}
+{{- $replicaCount := int .Values.replicaCount -}}
+{{- $endpoints := list -}}
+{{- range $i := until $replicaCount -}}
+{{- $endpoints = append $endpoints (printf "%s://%s-%d.%s.%s.svc.%s:%d" $clientProtocol $fullname $i $headlessService $releaseNamespace $clusterDomain $clientPort) -}}
 {{- end -}}
-{{- if and .Values.auth.client.secureTransport .Values.auth.client.useAutoTLS -}}
-{{- printf "%s" $autoCertsOption -}}
-{{- else if and .Values.auth.client.secureTransport (not .Values.auth.client.useAutoTLS) -}}
-{{- printf "%s" $certsOption -}}
-{{- if .Values.auth.client.enableAuthentication -}}
-{{- printf "%s" $caOption -}}
-{{- end -}}
-{{- end -}}
+{{- join "," $endpoints -}}
 {{- end -}}
 
 {{/*
-Return the etcd env vars ConfigMap name
-*/}}
-{{- define "etcd.envVarsCM" -}}
-{{- printf "%s" .Values.envVarsConfigMap -}}
-{{- end -}}
-
-{{/*
-Return the etcd env vars ConfigMap name
-*/}}
-{{- define "etcd.configFileCM" -}}
-{{- printf "%s" .Values.configFileConfigMap -}}
-{{- end -}}
-
-{{/*
-Return the proper Docker Image Registry Secret Names
-*/}}
-{{- define "etcd.imagePullSecrets" -}}
-{{/*
-Helm 2.11 supports the assignment of a value to a variable defined in a different scope,
-but Helm 2.9 and 2.10 does not support it, so we need to implement this if-else logic.
-Also, we can not use a single if because lazy evaluation is not an option
-*/}}
-{{- if .Values.global }}
-{{- if .Values.global.imagePullSecrets }}
-imagePullSecrets:
-{{- range .Values.global.imagePullSecrets }}
-  - name: {{ . }}
-{{- end }}
-{{- else if or .Values.image.pullSecrets .Values.volumePermissions.image.pullSecrets }}
-imagePullSecrets:
-{{- range .Values.image.pullSecrets }}
-  - name: {{ . }}
-{{- end }}
-{{- range .Values.volumePermissions.image.pullSecrets }}
-  - name: {{ . }}
-{{- end }}
-{{- end -}}
-{{- else if or .Values.image.pullSecrets .Values.volumePermissions.image.pullSecrets }}
-imagePullSecrets:
-{{- range .Values.image.pullSecrets }}
-  - name: {{ . }}
-{{- end }}
-{{- range .Values.volumePermissions.image.pullSecrets }}
-  - name: {{ . }}
-{{- end }}
-{{- end -}}
-{{- end -}}
-
-{{/*
-Compile all warnings into a single message, and call fail.
-*/}}
-{{- define "etcd.validateValues" -}}
-{{- $messages := list -}}
-{{- $messages := append $messages (include "etcd.validateValues.startFromSnapshot.existingClaim" .) -}}
-{{- $messages := append $messages (include "etcd.validateValues.startFromSnapshot.snapshotFilename" .) -}}
-{{- $messages := append $messages (include "etcd.validateValues.disasterRecovery" .) -}}
-{{- $messages := without $messages "" -}}
-{{- $message := join "\n" $messages -}}
-
-{{- if $message -}}
-{{-   printf "\nVALUES VALIDATION:\n%s" $message | fail -}}
-{{- end -}}
-{{- end -}}
-
-{{/* Validate values of etcd - an existing claim must be provided when startFromSnapshot is enabled */}}
-{{- define "etcd.validateValues.startFromSnapshot.existingClaim" -}}
-{{- if and .Values.startFromSnapshot.enabled (not .Values.startFromSnapshot.existingClaim) -}}
-etcd: startFromSnapshot.existingClaim
-    An existing claim must be provided when startFromSnapshot is enabled!!
-    Please provide it (--set startFromSnapshot.existingClaim="xxxx")
-{{- end -}}
-{{- end -}}
-
-{{/* Validate values of etcd - the snapshot filename must be provided when startFromSnapshot is enabled */}}
-{{- define "etcd.validateValues.startFromSnapshot.snapshotFilename" -}}
-{{- if and .Values.startFromSnapshot.enabled (not .Values.startFromSnapshot.snapshotFilename) -}}
-etcd: startFromSnapshot.snapshotFilename
-    The snapshot filename must be provided when startFromSnapshot is enabled!!
-    Please provide it (--set startFromSnapshot.snapshotFilename="xxxx")
-{{- end -}}
-{{- end -}}
-
-{{/* Validate values of etcd - persistence must be enabled when disasterRecovery is enabled */}}
-{{- define "etcd.validateValues.disasterRecovery" -}}
-{{- if and .Values.disasterRecovery.enabled (not .Values.persistence.enabled) -}}
-etcd: disasterRecovery
-    Persistence must be enabled when disasterRecovery is enabled!!
-    Please enable persistence (--set persistence.enabled=true)
-{{- end -}}
-{{- end -}}
-
-{{/*
-Return the proper image name (for the init container volume-permissions image)
-*/}}
-{{- define "etcd.volumePermissions.image" -}}
-{{- $registryName := .Values.volumePermissions.image.registry -}}
-{{- $repositoryName := .Values.volumePermissions.image.repository -}}
-{{- $tag := .Values.volumePermissions.image.tag | toString -}}
-{{/*
-Helm 2.11 supports the assignment of a value to a variable defined in a different scope,
-but Helm 2.9 and 2.10 doesn't support it, so we need to implement this if-else logic.
-Also, we can't use a single if because lazy evaluation is not an option
-*/}}
-{{- if .Values.global }}
-    {{- if .Values.global.imageRegistry }}
-        {{- printf "%s/%s:%s" .Values.global.imageRegistry $repositoryName $tag -}}
-    {{- else -}}
-        {{- printf "%s/%s:%s" $registryName $repositoryName $tag -}}
-    {{- end -}}
-{{- else -}}
-    {{- printf "%s/%s:%s" $registryName $repositoryName $tag -}}
-{{- end -}}
-{{- end -}}
-
-{{/*
-Return the proper Storage Class
+Return the storage class name
 */}}
 {{- define "etcd.storageClass" -}}
-{{/*
-Helm 2.11 supports the assignment of a value to a variable defined in a different scope,
-but Helm 2.9 and 2.10 does not support it, so we need to implement this if-else logic.
-*/}}
-{{- if .Values.global -}}
-    {{- if .Values.global.storageClass -}}
-        {{- if (eq "-" .Values.global.storageClass) -}}
-            {{- printf "storageClassName: \"\"" -}}
-        {{- else }}
-            {{- printf "storageClassName: %s" .Values.global.storageClass -}}
-        {{- end -}}
-    {{- else -}}
-        {{- if .Values.persistence.storageClass -}}
-              {{- if (eq "-" .Values.persistence.storageClass) -}}
-                  {{- printf "storageClassName: \"\"" -}}
-              {{- else }}
-                  {{- printf "storageClassName: %s" .Values.persistence.storageClass -}}
-              {{- end -}}
-        {{- end -}}
-    {{- end -}}
-{{- else -}}
-    {{- if .Values.persistence.storageClass -}}
-        {{- if (eq "-" .Values.persistence.storageClass) -}}
-            {{- printf "storageClassName: \"\"" -}}
-        {{- else }}
-            {{- printf "storageClassName: %s" .Values.persistence.storageClass -}}
-        {{- end -}}
-    {{- end -}}
+{{- if .Values.persistence.storageClass -}}
+  {{- if (eq "-" .Values.persistence.storageClass) -}}
+    {{- printf "storageClassName: \"\"" -}}
+  {{- else }}
+    {{- printf "storageClassName: %s" .Values.persistence.storageClass -}}
+  {{- end -}}
 {{- end -}}
 {{- end -}}
 
 {{/*
-Renders a value that contains template.
-Usage:
-{{ include "etcd.tplValue" ( dict "value" .Values.path.to.the.Value "context" $) }}
+Return image pull secrets
 */}}
-{{- define "etcd.tplValue" -}}
-    {{- if typeIs "string" .value }}
-        {{- tpl .value .context }}
-    {{- else }}
-        {{- tpl (.value | toYaml) .context }}
-    {{- end }}
+{{- define "etcd.imagePullSecrets" -}}
+{{- $pullSecrets := list }}
+{{- if .Values.global }}
+  {{- range .Values.global.imagePullSecrets }}
+    {{- $pullSecrets = append $pullSecrets . }}
+  {{- end }}
+{{- end }}
+{{- range .Values.image.pullSecrets }}
+  {{- $pullSecrets = append $pullSecrets . }}
+{{- end }}
+{{- if $pullSecrets }}
+imagePullSecrets:
+  {{- range $pullSecrets }}
+  - name: {{ . }}
+  {{- end }}
+{{- end }}
 {{- end -}}

commit f8540d7a53a2e48f03ede7be9de4141ed4f508a4

fix(etcd): restore statefulset.replicaCount in chart templates
Regression introduced in #572 (PTCI-3293): the CVE fix rewrote the
etcd chart and silently reverted .Values.statefulset.replicaCount back
to .Values.replicaCount in the StatefulSet template and helpers. The
DC config and Konk CR have always used statefulset.replicaCount (set
since #135), so etcd has been running as 1 replica even when clusters
were configured for HA (replicaCount: 3).

Fixes all three call sites in statefulset.yaml and _helpers.tpl, and
aligns etcd/values.yaml default to use the statefulset.replicaCount
key consistently.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

helm-charts/etcd/Chart.yaml
@@ -1,7 +1,7 @@
 apiVersion: v2
 name: etcd
 description: A custom etcd Helm chart using upstream etcd images
-version: 1.1.1
+version: 1.1.2
 appVersion: "3.7.1"
 type: application
 

helm-charts/etcd/templates/_helpers.tpl
@@ -107,7 +107,7 @@ Create the initial cluster string
 {{- $clusterDomain := .Values.clusterDomain -}}
 {{- $peerPort := int .Values.service.peerPort -}}
 {{- $peerProtocol := include "etcd.peerProtocol" . -}}
-{{- $replicaCount := int .Values.replicaCount -}}
+{{- $replicaCount := int .Values.statefulset.replicaCount -}}
 {{- $members := list -}}
 {{- range $i := until $replicaCount -}}
 {{- $members = append $members (printf "%s-%d=%s://%s-%d.%s.%s.svc.%s:%d" $fullname $i $peerProtocol $fullname $i $headlessService $releaseNamespace $clusterDomain $peerPort) -}}
@@ -125,7 +125,7 @@ Create the endpoints list for etcdctl
 {{- $clusterDomain := .Values.clusterDomain -}}
 {{- $clientPort := int .Values.service.port -}}
 {{- $clientProtocol := include "etcd.clientProtocol" . -}}
-{{- $replicaCount := int .Values.replicaCount -}}
+{{- $replicaCount := int .Values.statefulset.replicaCount -}}
 {{- $endpoints := list -}}
 {{- range $i := until $replicaCount -}}
 {{- $endpoints = append $endpoints (printf "%s://%s-%d.%s.%s.svc.%s:%d" $clientProtocol $fullname $i $headlessService $releaseNamespace $clusterDomain $clientPort) -}}

helm-charts/etcd/templates/statefulset.yaml
@@ -8,7 +8,7 @@ metadata:
   labels:
     {{- include "etcd.labels" . | nindent 4 }}
 spec:
-  replicas: {{ .Values.replicaCount }}
+  replicas: {{ .Values.statefulset.replicaCount }}
   serviceName: {{ include "etcd.headlessServiceName" . }}
   podManagementPolicy: Parallel
   updateStrategy:
@@ -88,7 +88,7 @@ spec:
               value: "{{ include "etcd.peerProtocol" . }}://0.0.0.0:{{ .Values.service.peerPort }}"
             - name: ETCD_INITIAL_ADVERTISE_PEER_URLS
               value: "{{ include "etcd.peerProtocol" . }}://$(ETCD_NAME).{{ include "etcd.headlessServiceName" . }}.{{ .Release.Namespace }}.svc.{{ .Values.clusterDomain }}:{{ .Values.service.peerPort }}"
-            {{- if gt (int .Values.replicaCount) 1 }}
+            {{- if gt (int .Values.statefulset.replicaCount) 1 }}
             - name: ETCD_INITIAL_CLUSTER
               value: {{ include "etcd.initialCluster" . | quote }}
             - name: ETCD_INITIAL_CLUSTER_TOKEN

helm-charts/etcd/values.yaml
@@ -15,7 +15,8 @@ image:
   pullSecrets: []
 
 ## @section etcd cluster parameters
-replicaCount: 1
+statefulset:
+  replicaCount: 1
 
 ## @section etcd configuration
 etcd: